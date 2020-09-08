a90fceba98436726fc2f04f95ed998fc5dad02e6
aiaoyang <aiaoyang@outlook.com>
2020-09-08 20:00:36 +0800

    fix

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-// #![feature(const_fn)]
 mod cpu;
 mod error;
 mod mem;
@@ -7,7 +6,6 @@ mod read;
 use cpu::*;
 use mem::*;
 
-// }
 fn main() {
     let pids = std::env::args().collect::<Vec<String>>();
     let pid = pids.get(1).unwrap().as_str();
@@ -15,9 +13,13 @@ fn main() {
     println!("pid is : {}", &pid);
     for _ in 0..10 {
         let process_usage = process_cpu_usage(pid);
+
         let system_cpu_usage = system_cpu_usage();
+
         let process_mem_usage = process_mem_usage(pid);
+
         let system_mem_usage = system_mem_usage();
+
         println!("process_cpu_usage\t->\t{:?}%\nsystem_cpu_usage\t->\t{}%\nprocess_mem_usage\t->\t{}%\nsystem_mem_usage\t->\t{}%", process_usage,
 				system_cpu_usage,
 				process_mem_usage,
